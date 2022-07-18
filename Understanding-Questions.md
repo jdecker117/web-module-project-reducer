50828eddeab5e72686272f69ce36ccfaf4fff025
jason decker <jdeck@MacBook-Pro.local>
2022-07-18 19:02:13 -0400

    Completed majority of MVP tasks

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,8 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
+* Triggers the handleAddOne method which dispatches(updates state) with the imported addOne function.
+* The addOne function within notifies the reducer function located in reducers/index.js that the case is add one. This case adds one to the state property, total.
 ...
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import reducer, {initialState} from './../reducers/index'
+import {addOne, applyNumber, changeOP, clearDisplay} from './../actions/index'
 
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+console.log(reducer)
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { operation, memory, total } = state;
+
+  // const handleAddOne = () => {
+  //   dispatch(addOne())
+  // }
+
+  const handleApplyNumber = (number) => {
+    dispatch(applyNumber(number))
+  }
+
+  const handleChangeOp = (operation) => {
+    dispatch(changeOP(operation))
+  }
+
+  const handleClear = () => {
+    dispatch(clearDisplay())
+  }
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,10 +40,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {operation}</span>
+              <span id="memory"><b>Memory:</b> {memory}</span>
             </div>
             
             <div className="row">
@@ -29,31 +53,31 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={() => handleApplyNumber(1)} value={1}/>
+              <CalcButton onClick={() => handleApplyNumber(2)} value={2}/>
+              <CalcButton onClick={() => handleApplyNumber(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={() => handleApplyNumber(4)} value={4}/>
+              <CalcButton onClick={() => handleApplyNumber(5)} value={5}/>
+              <CalcButton onClick={() => handleApplyNumber(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={() => handleApplyNumber(7)} value={7}/>
+              <CalcButton onClick={() => handleApplyNumber(8)} value={8}/>
+              <CalcButton onClick={() => handleApplyNumber(9)} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={() => handleChangeOp("+")} value={"+"}/>
+              <CalcButton onClick={() => handleChangeOp("*")} value={"*"}/>
+              <CalcButton onClick={() => handleChangeOp("-")} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={handleClear} value={"CE"}/>
             </div>
 
           </form>
